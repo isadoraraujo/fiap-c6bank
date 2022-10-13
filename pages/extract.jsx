ad523a6969da40e7e5028536719bf0c2651661aa
Isadora Araújo <isadoraraujo13@gmail.com>
2022-10-13 09:54:48 -0300

    deploy

---
+++ pages/extract.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Extract() {
+    return (
+        <div>
+            <h1>
+                extract
+            </h1>
+        </div>
+    )
+}
