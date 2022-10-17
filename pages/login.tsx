de569b2f9b910cf2af3ec26da921ca301faa02a0
Isadora Araújo <isadoraraujo13@gmail.com>
2022-10-17 11:29:41 -0300

    login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,47 +1,83 @@
-import { Typography } from '@mui/material';
-import React from 'react'
-import Link from 'next/link';
+import React, { useState, useEffect, FormEvent } from 'react'
+import {
+    Typography, Container, CssBaseline, Box, TextField,
+    Checkbox, Button, FormControlLabel
+} from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import { Copyright } from '../components/utils/Copyright';
+import Snackbar from '../components/utils/Snackbar';
 
-type CopyProps = {
-    site: string,
-    sx?: object
-}
-
-function Copyright(props: CopyProps) {
-    return (
-        <Typography>
-            {'Copyright ©️'}
-            <Link color='inherit' href='https://avanade.com.br'>
-                {props.site}
-            </Link>
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    )
-}
-
-const theme = createTheme()
+const theme = createTheme();
 
 export default function LoginPage() {
+    const [empresa, setEmpresa] = useState<string>('');
+    const [nome, setNome] = useState('');
+    const [contador, setContador] = useState<number>(5);
+    const [error, setError] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>('');
+    const [email, setEmail] = useState<string | undefined | null | FormDataEntryValue>('');
+    const [password, setPassword] = useState<string | undefined | null | FormDataEntryValue>('');
+    const [open, setOpen] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (contador == 0) {
+            document.title = `Executando useEffect a primeira vez ${contador}`;
+        } else {
+            document.title = `Executando useEffect a cada alteração ${contador}`;
+        }
+        console.log(`Executando useEffect a cada chamada ${contador}`);
+    }, [contador]);
+
+
+    useEffect(() => {
+        if (nome.length > 0) {
+            console.log(`Executando useEffect mudando para maiúsculo ${nome}`);
+        }
+    }, [nome])
+
+
+    useEffect(() => {
+
+        if (password && password.length < 6) {
+            setError(true);
+            setErrorMessage('A senha deve ter no mínimo 6 caracteres');
+        } else if (password) {
+            setError(false);
+            setErrorMessage('');
+            setOpen(true);
+        }
+
+    }, [password]);
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+
+        setEmail(data.get('email'));
+        setPassword(data.get('password'));
+    }
+
     return (
         <ThemeProvider theme={theme}>
-            <Container component='main' maxWidth='xs'>
+            <Container component="main" maxWidth="xs">
                 <CssBaseline />
+                
                 <Box sx={{ mt: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <Typography component='h1' variant='h5'>
+                    <Typography component="h1" variant="h5">
                         Login
                     </Typography>
-                    <Box component='form' onSubmit={(e) => { console.log('enviou') }}>
-                        <TextField margin='normal' required fullWidth id='email' label='Digite o email' name='email' autoComplete='email' autoFocus />
-                        <TextField margin='normal' required fullWidth id='password' type='password' label='Digite a sua senha' name='password' autoComplete='current-password' autoFocus />
+                    <Box component="form" onSubmit={handleSubmit}>
+                        <TextField margin="normal" required fullWidth id="email" label="Digite o e-mail" name="email" autoComplete="email" autoFocus />
+                        <TextField margin="normal" required fullWidth id="password" type="password" label="Digite a senha" name="password" autoComplete="current-password" autoFocus />
+                        <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Lembrar de mim" />
+                        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Entrar</Button>
+
+                        {error && <Typography color="error">{errorMessage}</Typography>}
                     </Box>
                 </Box>
-                <Copyright site='avanade.com.br' sx={{ mt: 8, mb: 4 }} />
+
+                <Copyright site="www.avanade.com.br" sx={{ mt: 8, mb: 4 }} />
+                {open && <Snackbar open={open} hide={6} message='Usuário autenticado com sucesso...Aguarde...' />}
             </Container>
         </ThemeProvider>
     )
